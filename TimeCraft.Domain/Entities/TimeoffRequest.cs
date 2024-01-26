19534271b613a10cefdf2b55732e5b21cfa0d868
albion599 <albionpaqarizi1@gmail.com>
2024-01-26 23:35:25 +0100

    Add Entitites part 2

---
+++ TimeCraft.Domain/Entities/TimeoffRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCraft.Domain.Entities
+{
+    public class TimeoffRequest : BaseEntity
+    {
+        public int EmployeeId { get; set; }
+
+        public Employee? Employee { get; set; }
+
+        public string Type { get; set; } // TimeoffType enum
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Status { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
