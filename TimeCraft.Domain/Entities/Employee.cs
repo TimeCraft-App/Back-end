4d544051a5f14dd3edb25ed8c9f030aecd788b4b
albion599 <albionpaqarizi1@gmail.com>
2024-01-26 22:49:36 +0100

    Add Some Entities

---
+++ TimeCraft.Domain/Entities/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCraft.Domain.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+
+        public int? SalaryId { get; set; }
+
+        public Salary? Salary { get; set; }
+
+        public int PositionId { get; set; }
+        public Position? Position { get; set; }
+    }
+}
---
+++ TimeCraft.Domain/Entities/Project.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCraft.Domain.Entities
+{
+    public class Project : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public int? ManagerId { get; set; }
+
+        public Employee? Manager { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
